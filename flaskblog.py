6687024e700140fac76817e2eff1924169b263fe
Nick Roshdieh <nickroshd@gmail.com>
2021-11-28 22:14:34 -0500

    added an about page routing

--- flaskblog.py
+++ flaskblog.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return "<h1>Hello, Internet!</h1>"
 
+
+@app.route("/about")
+def about_page():
+    return "<p>I need to test this framework to see what's going on</p>"
+
 if __name__ == '__main__':
     app.run(debug=True)
