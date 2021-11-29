18383c112baa3cea2fdf2da6d0f93c50ebb93555
Nick Roshdieh <nickroshd@gmail.com>
2021-11-28 22:46:08 -0500

    added tempaltes for home and about

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+    'author' : 'Nick R',
+    'title': 'post 1',
+    'content': 'this is the first post',
+    'date_posted': 'June 9, 1969'
+    },
+    {
+    'author' : 'John Smith',
+    'title': 'post 2',
+    'content': 'this is the second',
+    'date_posted': 'March 9, 1999'
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home_page():
-    return "<h1>Hello, Internet!</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about_page():
-    return "<p>I need to test this framework to see what's going on</p>"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
