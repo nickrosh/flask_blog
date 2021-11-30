1c9fbe6dd2325f569ca5f41c547255d0da3d0726
Nick Roshdieh <nickroshd@gmail.com>
2021-11-29 21:58:10 -0500

    added login and registration pages

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '56ee563ebf03882044dd258d8cb54cc6'
 
 posts = [
     {
@@ -26,6 +29,18 @@ def home_page():
 @app.route("/about")
 def about_page():
     return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!')
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
